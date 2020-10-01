c26d16519fedea373738f81d97448f6c8454ad69
kirperov <kirill.petrov.centrale@gmail.com>
2020-10-01 17:55:49 +0200

    get clicked card event

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,8 @@ import React from 'react'
 import './Card.css'
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback }) => (
-    <div className={`card ${feedback}`}>
+const Card = ({ card, feedback, onClick }) => (
+    <div className={`card ${feedback}`} onClick={() =>onClick(card)}>
          <span className="symbol">
              { feedback === 'hidden' ? HIDDEN_SYMBOL : card }
          </span>
